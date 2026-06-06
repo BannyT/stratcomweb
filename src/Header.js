dc495a8973b007046cc99ead83920a2d6f7d5514
BannyT <tumusiimebarnabus@gmail.com>
2026-06-06 14:50:01 +0300

    code updates

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+  return (
+    <div>
+         <h1 className='header'>this is my header</h1>
+    </div>
+  )
+}
+
+export default Header
