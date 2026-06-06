eb588b37b299d409cab13c5186a3d8b8e66ce6ca
BannyT <tumusiimebarnabus@gmail.com>
2026-06-06 18:58:04 +0300

    updates code for interns

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Carousel from './Carousel';
+import Services from './Services';
 
 function App() {
   return (
     <div className="App">
              <Header/>
+             <Carousel autoPlayInterval={5000} />
+             <Services/>
     </div>
   );
 }
