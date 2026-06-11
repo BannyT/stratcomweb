ad7e40862b41b1a6546b3834422f733bc1bc68be
BannyT <tumusiimebarnabus@gmail.com>
2026-06-11 12:00:50 +0300

    updates intern code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from './Header';
 import Carousel from './Carousel';
 import Services from './Services';
+import AboutUs from './AboutUS';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
              <Header/>
-             {/* <Carousel autoPlayInterval={5000} /> */}
-             {/* <Services/> */}
+             <Carousel autoPlayInterval={5000} />
+             <AboutUs/>
+             <Services/>
+             <Footer/>
     </div>
   );
 }
