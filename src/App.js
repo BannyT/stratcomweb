5499410a470aa7213408cea59656488a22e1b7a6
BannyT <tumusiimebarnabus@gmail.com>
2026-06-20 11:20:55 +0300

    updates code

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ import Carousel from './Carousel';
 import Services from './Services';
 import AboutUs from './AboutUS';
 import Footer from './Footer';
+import Home from './Home'
 import Login from './Login';
 import Signup from './Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-             {/* <Homepage/> */}
-             <Header/>
-             <Carousel autoPlayInterval={5000} />
-             <AboutUs/>
-              <Services/>
-              <Login/>
-              <Signup/>
+            <Router> 
+                <Routes>
+                   <Route path="/*" element={
+                    <>
+                      <Header/> 
+                      <Routes>
+                           <Route path='/' element={<Home/>}/>
+                           <Route path='/about' element={<AboutUs/>}/>
+                           <Route path='/services' element={<Services/>}/>
+                      </Routes>
+                    </>
+                   } />  
+                </Routes>
+            </Router>
+                    
+              {/* <Login/>
+              <Signup/> */}
              <Footer/> 
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from './Header'
+import Carousel from './Carousel';
+import Services from './Services';
+import AboutUs from './AboutUS';
+import Footer from './Footer';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+    
+      <Carousel autoPlayInterval={5000} />
+      <AboutUs/>
+      <Services/>
+      {/* <Footer/>  */}
+    </div>
   )
 }
 
