9e9bf15aa8b030ef22a93ed29dd2cef746fb7dd5
BannyT <tumusiimebarnabus@gmail.com>
2026-06-27 11:20:11 +0300

    updates  data

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZIK7MGSeJJplSHNszTft9FGXbbkkH2HQ",
+    authDomain: "stratcom-8d562.firebaseapp.com",
+    projectId: "stratcom-8d562",
+    storageBucket: "stratcom-8d562.firebasestorage.app",
+    messagingSenderId: "437925309004",
+    appId: "1:437925309004:web:20b126609920e5be1ea2e9",
+    measurementId: "G-N69MC6WNFR"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile };
